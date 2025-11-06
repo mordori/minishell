9ac4f1859436e83fbc4d1603759c1d30fabba02c
Janne Valkama <jvalkama@c1r2p5.hive.fi>
2025-11-06 19:42:02 +0200

    exit supports exit codes like bash

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: jvalkama <jvalkama@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 17:26:59 by jvalkama          #+#    #+#             */
-/*   Updated: 2025/11/06 18:48:18 by jvalkama         ###   ########.fr       */
+/*   Updated: 2025/11/06 19:41:18 by jvalkama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 #include "errors.h"
 #include "io.h"
 
-static inline bool	is_numbers(char *str);
+static inline bool	is_numbers(const char *str, int *i);
+static inline void	warn_numeric(t_minishell *ms, const char *arg);
+static inline bool	is_valid(t_minishell *ms, const char *a, uint32_t *status);
 
 int	exi(t_minishell *ms, t_node *node)
 {
-	int	status;
+	uint32_t	status;
 
-	(void) node;
 	try_write_endl(ms, STDOUT_FILENO, "exit");
 	status = ms->state.exit_status;
 	if (node->cmd.argc > 2)
@@ -31,28 +32,54 @@ int	exi(t_minishell *ms, t_node *node)
 	}
 	if (node->cmd.args[1])
 	{
-		if (is_numbers(node->cmd.args[1]) == false)
-			warning(ms, \
-str_join(ms, "exit: ", \
-str_join(ms, node->cmd.args[1], ": numeric argument required", \
-VOLATILE), VOLATILE));
-		else
-			status = ft_atoi(node->cmd.args[1]);
+		if (is_valid(ms, node->cmd.args[1], &status) == false)
+			status = ERROR_BUILTIN;
 	}
 	clean(ms);
-	exit(status);
+	exit((char)status);
 }
 
-static inline bool	is_numbers(char *str)
+static inline bool	is_valid(t_minishell *ms, const char *a, uint32_t *status)
 {
-	int			i;
+	int		i;
 
 	i = 0;
-	while (str[i])
+	if (is_numbers(a, &i) == false)
 	{
-		if (ft_isdigit(str[i]) == false)
+		warn_numeric(ms, a);
+		return (false);
+	}
+	if (i > 11)
+	{
+		warn_numeric(ms, a);
+		return (false);
+	}
+	else
+		*status = ft_atouint32_t_base(a, "0123456789");
+	return (true);
+}
+
+static inline bool	is_numbers(const char *str, int *i)
+{
+	if (str[0] == '-' || str[0] == '+')
+	{
+		(*i)++;
+		if (!str[*i])
 			return (false);
-		i++;
+	}
+	while (str[*i])
+	{
+		if (ft_isdigit(str[*i]) == false)
+			return (false);
+		(*i)++;
 	}
 	return (true);
+}
+
+static inline void	warn_numeric(t_minishell *ms, const char *arg)
+{
+	warning(ms, \
+str_join(ms, "exit: ", \
+str_join(ms, arg, ": numeric argument required", \
+VOLATILE), VOLATILE));
 }
