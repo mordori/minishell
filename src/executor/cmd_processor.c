57508dad8012fca33976f691fbbfc3f3e764b712
Mika Yli-Pentti <mika.ylipentti@gmail.com>
2025-11-22 16:40:47 +0200

    Fixed $_ to display the last arg instead of the first

--- src/executor/cmd_processor.c
+++ src/executor/cmd_processor.c
@@ -6,7 +6,7 @@
 /*   By: myli-pen <myli-pen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 16:38:28 by jvalkama          #+#    #+#             */
-/*   Updated: 2025/11/21 21:19:06 by myli-pen         ###   ########.fr       */
+/*   Updated: 2025/11/22 16:38:22 by myli-pen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,25 +50,37 @@ int	exec_extern(t_minishell *ms, t_node *node)
 	return (ERROR);
 }
 
-void	update_env_lastcmd(t_minishell *ms, char *cmd, t_builtin builtin)
+static inline char	*get_last_arg(t_minishell *ms, char **args)
 {
+	char	*result;
 	t_env	*shell_v;
+
+	while (*args)
+	{
+		result = *args;
+		++args;
+	}
+	if (!ft_strcmp(result, "env"))
+	{
+		shell_v = envll_findkey(&ms->state, "SHELL");
+		if (shell_v)
+			result = str_join(ms, \
+str_join(ms, shell_v->value, "/src/builtin/", VOLATILE), result, VOLATILE);
+	}
+	return (result);
+}
+
+void	update_env_lastcmd(t_minishell *ms, char **args)
+{
 	t_env	*lastcmd_v;
 
 	copy_env_to(VOLATILE, ms);
 	arena_reset(&ms->vars);
-	if (builtin)
-	{
-		shell_v = envll_findkey(&ms->state, "SHELL");
-		if (shell_v)
-			cmd = str_join(ms, \
-str_join(ms, shell_v->value, "/src/builtin/", VOLATILE), cmd, VOLATILE);
-	}
 	lastcmd_v = envll_findkey(&ms->state, "_");
 	if (lastcmd_v)
-		replace_value(lastcmd_v, cmd);
+		replace_value(lastcmd_v, get_last_arg(ms, args));
 	else
-		var_to_node(ms, str_join(ms, "_=", cmd, VOLATILE), &ms->state.env);
+		var_to_node(ms, str_join(ms, "_=", get_last_arg(ms, args), VOLATILE), &ms->state.env);
 	copy_env_to(PERSISTENT, ms);
 	ms->state.envp = envll_to_envp(ms, ms->state.env);
 }
