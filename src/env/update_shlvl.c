80ba2d568e0690cec6bde151d44510236c037efb
Mika Yli-Pentti <mika.ylipentti@gmail.com>
2025-11-25 02:54:58 +0200

    Replaced isatty calls after init to mode

--- src/env/update_shlvl.c
+++ src/env/update_shlvl.c
@@ -6,7 +6,7 @@
 /*   By: myli-pen <myli-pen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 11:55:32 by jvalkama          #+#    #+#             */
-/*   Updated: 2025/11/24 19:45:03 by myli-pen         ###   ########.fr       */
+/*   Updated: 2025/11/25 02:52:31 by myli-pen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,8 @@ void	update_shlvl(t_minishell *ms, char *delim, char **value)
 
 	ft_memset(buf, 0, INT32_LENGTH + 1);
 	val = ft_strtol(delim, &c);
-	if (c || !isatty(STDIN_FILENO))
+	if (c || ms->mode == AUTONOMOUS)
 	{
-		if (!isatty(STDIN_FILENO))
-			errno = 0;
 		*value = str_dup(ms, "1", PERSISTENT);
 		return ;
 	}
